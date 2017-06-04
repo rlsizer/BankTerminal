1e6684a3a41e3e148dc239b2b69c59dcf4b1766a
Ryan Sizer <rlsizer@fullsail.edu>
2017-06-04 14:40:56 -0400

    Set up CheckingAccount and Customer classes.

---
+++ RyanSizer_CE02/CheckingAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RyanSizer_CE02
+{
+    class CheckingAccount
+    {
+
+
+        //fields
+        private decimal accountBal;
+        private int accountNum;
+
+
+        //Properties
+        public decimal AccountBal
+        {
+            get { return this.accountBal; }
+
+        }
+
+
+        //Constructors
+        public CheckingAccount(decimal accountBal, int accountNum)
+        {
+            this.accountBal = accountBal;
+            this.accountNum = accountNum;
+        }
+
+
+    }
+}
---
+++ RyanSizer_CE02/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RyanSizer_CE02
+{
+    class Customer
+    {
+
+        //fields
+        private CheckingAccount account;
+        private string customerName;
+
+
+        //Properties
+        public CheckingAccount Account
+        {
+            get { return this.account; }
+
+        }
+
+
+        //Constructors
+        public Customer(string customerName)
+        {
+            this.customerName = customerName;
+
+        }
+
+    }
+}
